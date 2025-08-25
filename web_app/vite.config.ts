56d84cce5d6556c2c10a846ce5bbc5289081c31b
Joe Price <jc2price@gmail.com>
2025-08-25 11:34:23 -0400

    add pwa

--- web_app/vite.config.ts
+++ web_app/vite.config.ts
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from "vite-plugin-wasm";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [react(), wasm()],
+    plugins: [
+        react(),
+        wasm(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico'],
+            manifest: {
+                name: 'SantoriniAI',
+                short_name: 'SantoriniAI',
+                theme_color: '#cfecf7',
+                icons: [
+                    {
+                        src: 'img-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any'
+                    },
+                ],
+            },
+        })
+    ],
     worker: {
         format: "es",
     },
