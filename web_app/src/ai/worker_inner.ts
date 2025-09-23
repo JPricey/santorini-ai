d20ecb4f89a162c840d0832379f80a8a4646ae7e
Joe Price <jc2price@gmail.com>
2025-09-23 07:50:40 -0400

    Rename tab

--- web_app/src/ai/worker_inner.ts
+++ web_app/src/ai/worker_inner.ts
@@ -1,14 +1,11 @@
-// import init, { WasmApp } from "../../pkg/wasm_app";
-
 async function initializeWasmWorker() {
     self.postMessage('startup');
-    const wasm = await import('../../pkg/wasm_app'); // adjust path as needed
+    const wasm = await import('../../pkg/wasm_app');
     await wasm.default();
 
     const worker = new wasm.WasmApp();
 
     self.onmessage = async e => {
-        // console.log(new Date().toISOString(), 'Message received from main thread: ', e.data);
         const thinkingResponse = worker.computeNextMove(e.data[0], e.data[1]);
         self.postMessage(thinkingResponse);
     };
