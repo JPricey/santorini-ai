810d98a42ffa0dea2560709f6067805fb077129a
Joe Price <jc2price@gmail.com>
2025-11-01 20:47:05 -0700

    Add pegasus to UIs

--- web_app/src/common/game_state.tsx
+++ web_app/src/common/game_state.tsx
@@ -34,12 +34,13 @@ export const God = {
     Charon: 'charon',
     Eros: 'eros',
     Hippolyta: 'hippolyta',
+    Pegasus: 'pegasus',
     Scylla: 'scylla',
     Selene: 'selene',
 } as const;
 export type GodType = typeof God[keyof typeof God];
 
-export const WIP_GODS: Set<GodType> = new Set([God.Charon, God.Eros, God.Hippolyta, God.Scylla, God.Selene]);
+export const WIP_GODS: Set<GodType> = new Set([God.Charon, God.Eros, God.Hippolyta, God.Pegasus, God.Scylla, God.Selene]);
 
 export const Square = {
     A5: 0,
