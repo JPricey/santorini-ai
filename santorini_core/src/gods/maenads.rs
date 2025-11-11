391bddc4a767452ea69af83f47013f43e0c19074
Joe Price <jc2price@gmail.com>
2025-11-10 21:16:31 -0800

    Maenads can dance vs hera

--- santorini_core/src/gods/maenads.rs
+++ santorini_core/src/gods/maenads.rs
@@ -3,13 +3,18 @@ use crate::{
     board::{BoardState, FullGameState},
     build_god_power_movers,
     gods::{
-        build_god_power_actions, generic::{
-            GenericMove, GodMove, MoveData, MoveGenFlags, ScoredMove, LOWER_POSITION_MASK, MOVE_IS_WINNING_MASK, NULL_MOVE_DATA, POSITION_WIDTH
-        }, god_power, move_helpers::{
+        FullAction, GodName, GodPower, HistoryIdxHelper, PartialAction, StaticGod,
+        build_god_power_actions,
+        generic::{
+            GenericMove, GodMove, LOWER_POSITION_MASK, MOVE_IS_WINNING_MASK, MoveData,
+            MoveGenFlags, NULL_MOVE_DATA, POSITION_WIDTH, ScoredMove,
+        },
+        god_power,
+        move_helpers::{
             build_scored_move, get_generator_prelude_state, get_standard_reach_board,
             get_worker_end_move_state, get_worker_next_build_state, get_worker_next_move_state,
             get_worker_start_move_state, is_mate_only, is_stop_on_mate, push_winning_moves,
-        }, FullAction, GodName, GodPower, HistoryIdxHelper, PartialAction, StaticGod
+        },
     },
     persephone_check_result,
     player::Player,
@@ -203,7 +208,7 @@ pub(super) fn maenads_move_gen<const F: MoveGenFlags, const MUST_CLIMB: bool>(
 
         // Win by dancing
         if !prelude.is_against_harpies {
-            let dance_wins = worker_next_moves.worker_moves & mad_spots & prelude.win_mask;
+            let dance_wins = worker_next_moves.worker_moves & mad_spots;
 
             if dance_wins.is_not_empty() {
                 let unblocked_squares =
